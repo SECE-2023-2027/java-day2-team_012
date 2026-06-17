00fc8cdabef2eba877bf5f956ca122df79b4f510
KAVIYARASU  M <155162886+KAVIYARASUCCE027@users.noreply.github.com>
2026-06-17 07:40:07 +0530

    Create P15.java

---
+++ P15.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StringCompression {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String compressed = "";
+        int count = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (i < str.length() - 1 &&
+                    str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                compressed += str.charAt(i) + "" + count;
+                count = 1;
+            }
+        }
+
+        System.out.println(compressed);
+    }
+}
